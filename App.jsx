8e7011350ff1a122b80e3c498fbaf7d4ade2f628
Vikram Kangotra <vikramkangotra8055@gmail.com>
2023-09-21 23:09:05 +0530

    init commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function App() {
+  return (
+      <View className="items-center justify-center flex-1">
+        <Text className="text-4xl">Hello World</Text>
+      </View>
+  );
+}
+
+export default App;
