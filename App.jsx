f970750aa53da387a2c759d3792363408b4fbd11
Vikram Kangotra <vikramkangotra8055@gmail.com>
2023-09-22 15:20:08 +0530

    Added some code

--- App.jsx
+++ App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+function Card({name}) {
+    return (
+        <View className="justify-center w-9/12 text-2xl bg-[#434556] rounded-lg shadow-lg h-1/5 shadow-black">
+            <Text className="text-3xl">{name}</Text>
+            <Text className="text-3xl">{name}</Text>
+        </View>
+    )
+}
+
 function App() {
   return (
-      <View className="items-center justify-center flex-1">
-        <Text className="text-4xl">Hello World</Text>
+      <View className="items-center justify-center flex-1 bg-[#434556] gap-3">
+        <Card name="vikram" />
+        <Card name="Disha" />
+        <Card name="Got" />
       </View>
   );
 }
 
 export default App;
+
