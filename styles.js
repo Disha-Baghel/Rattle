7034a8d67ed82a3bda5017548c33df37ba672604
disha-baghel <bagheldisha708@gmail.com>
2023-09-23 14:30:35 +0530

    code for styling of image

---
+++ styles.js
@@ -0,0 +1,22 @@
+const styles = {
+    container: {
+      backgroundColor: '#434556', // Background color
+      width: 60, // Width of the container
+      height: 60, // Height of the container
+      borderRadius: 16, // Rounded corners
+      shadowColor: '#fff', // Shadow color
+      shadowOffset: { width: 0, height: 2 }, // Shadow offset
+      shadowOpacity: 0.3, // Shadow opacity
+      shadowRadius: 4, // Shadow radius
+      justifyContent: 'center', // Center content vertically
+      alignItems: 'center', // Center content horizontally
+    },
+    image: {
+      width: 60, // Width of the image
+      height: 60, // Height of the image
+      resizeMode: 'cover', // Adjust image size mode as needed
+    
+    },
+  };
+  
+  export default styles;
